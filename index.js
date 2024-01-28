7e1744e7fdadade151fb340d9ca79d9eed8a69f7
shivani <chovatiyashivani@gmail.com>
2024-01-28 18:06:02 +0530

    d

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
+dotenv.config();
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.get("/", (req, res) =>
   res.send("Welcome to Shivani's first Node app! Keep things Jiggy ;-) ")
